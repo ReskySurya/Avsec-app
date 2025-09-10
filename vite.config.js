1f03578dd3473c1ea35bd57dba31a60812d185ac
AndhikaW <andhikaw61@gmail.com>
2025-09-10 23:01:22 +0700

    [update] - vite

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,3 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    server: {
-        host: '0.0.0.0',
-        port: 5173, // atau port bebas lainnya
-        strictPort: true,
-        hmr: {
-            host: '192.168.0.116', // <- Ganti dengan IP lokal kamu
-        },
-    },
-});
-
-
 // import { defineConfig } from 'vite';
 // import laravel from 'laravel-vite-plugin';
 
@@ -29,4 +8,25 @@ export default defineConfig({
 //             refresh: true,
 //         }),
 //     ],
+//     server: {
+//         host: '0.0.0.0',
+//         port: 5173, // atau port bebas lainnya
+//         strictPort: true,
+//         hmr: {
+//             host: '192.168.0.126', // <- Ganti dengan IP lokal kamu
+//         },
+//     },
 // });
+
+
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
